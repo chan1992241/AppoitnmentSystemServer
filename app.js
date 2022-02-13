12dd4f96ecf0b3701dd1f81a5aba72dc0260fe94
chan1992241 <chan1992241@gmail.com>
2022-02-13 21:04:46 +0800

    remove mongoose

--- app.js
+++ app.js
@@ -1,18 +1,9 @@
 const express = require("express");
 const app = express();
-const mongoose = require('mongoose');
 
 // use ejs-locals for all ejs templates (refer to ejs-mate doc)
 app.use(express.json()) // for parsing application/json
 app.use(express.urlencoded({ extended: true }))
-
-const dbURL = 'mongodb://localhost:27017/yelp-camp';
-mongoose.connect(dbURL);
-const db = mongoose.connection;
-db.on('error', console.error.bind(console, 'connection error:')); //check database connection
-db.once('open', function () {
-    console.log("database connected");
-});
 
 app.get("/", (req, res) => {
     res.send("Hello")
